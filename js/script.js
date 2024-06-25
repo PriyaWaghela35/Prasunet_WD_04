e5383166fb65b09217d0d4b7926a6dde17aa8734
PriyaWaghela35 <119736019+PriyaWaghela35@users.noreply.github.com>
2024-06-25 12:38:02 +0530

    Initial Commit
    Adding Essential Files

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*------------------------ TYPED ANIMATION--------------------------*/
+var typed = new Typed(".typing",{
+    strings:["Artificial Intelligence and Data Science Engineer", "Web Developer", "Data Analyst", "Programmer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
